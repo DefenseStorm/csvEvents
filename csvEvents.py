4ce9c82e240083d1a4ed43217201d66d5427e95e
Alex Hernandez <alex@defensestorm.com>
2024-02-02 15:18:41 -0500

    first commit

---
+++ csvEvents.py
@@ -0,0 +1,235 @@
+#!/usr/bin/env python3
+
+import sys,os,getopt
+import traceback
+import os
+import fcntl
+import json
+import requests
+from requests.auth import HTTPBasicAuth
+import time
+import re
+from datetime import datetime, timedelta
+import base64
+from os import listdir
+from os.path import isfile, join
+import csv
+
+sys.path.insert(0, './ds-integration')
+from DefenseStorm import DefenseStorm
+
+from html.parser import HTMLParser
+
+class integration(object):
+
+    JSON_field_mappings_pdw = {
+    }
+    JSON_field_mappings_securenow = {
+    }
+
+
+    def checkDirectory(self):
+        try:
+            file_list = [f for f in listdir(self.watch_dir) if isfile(join(self.watch_dir, f))]
+        except Exception as e:
+            self.ds.logger.error("Failed to read directory: %s" %self.watch_dir)
+            self.ds.logger.error("Exception {0}".format(str(e)))
+            self.ds.logger.error("%s" %(traceback.format_exc().replace('\n',';')))
+            return None
+        for file_name in file_list:
+            data_type = None
+            self.ds.logger.info("Reading file: %s" %(file_name))
+
+            if self.securenow_prefix in file_name:
+                self.ds.logger.info('Found SecureNow file: %s' %file_name)
+                data_type = 'securenow'
+            elif self.pdw_prefix in file_name:
+                self.ds.logger.info('Found PDW file: %s' %file_name)
+                data_type = 'pdw'
+            else:
+                self.ds.logger.error('Found unexpected file. Skipping: %s' %file_name)
+                continue
+
+            event_list = self.readCSVFile(self.watch_dir + '/' + file_name)
+            if event_list == None:
+                self.ds.logger.error('Failed to read csv data for file: %s' %file_name)
+                continue
+
+            if data_type == 'securenow':
+                for event in event_list:
+                    event['app_name'] = 'securenow'
+                    self.ds.writeJSONEvent(event, JSON_field_mappings = self.JSON_field_mappings_securenow)
+            elif data_type == 'pdw':
+                for event in event_list:
+                    event['app_name'] = 'pdw'
+                    self.ds.writeJSONEvent(event, JSON_field_mappings = self.JSON_field_mappings_pdw)
+            else:
+                self.ds.logger.error('Found data type. Skipping: %s' %data_type)
+                continue
+
+            self.ds.logger.info('Backing up file: %s to directory %s' %(file_name, self.backup_dir))
+            try:
+                os.rename(self.watch_dir + '/' + file_name, self.backup_dir + '/' + file_name)
+            except Exception as e:
+                self.ds.logger.error("Failed to backup file: %s" %file_name)
+                self.ds.logger.error("Exception {0}".format(str(e)))
+                continue
+
+        return True
+
+    def readCSVFile(self, csvFilePath):
+        data = []
+        try:
+            with open(csvFilePath, encoding='utf-8') as csvf:
+                csvReader = csv.DictReader(csvf)
+                for row in csvReader:
+                    data.append(row)
+        except Exception as e:
+            self.ds.logger.error("Failed to read CSV file: %s" %csvFilePath)
+            self.ds.logger.error("Exception {0}".format(str(e)))
+            self.ds.logger.error("%s" %(traceback.format_exc().replace('\n',';')))
+            return None
+        return data
+
+    def csv_main(self): 
+
+        self.watch_dir = self.ds.config_get('csv', 'watch_dir')
+        self.backup_dir = self.ds.config_get('csv', 'backup_dir')
+        self.state_dir = self.ds.config_get('csv', 'state_dir')
+        self.securenow_prefix = self.ds.config_get('csv', 'securenow_prefix')
+        self.pdw_prefix = self.ds.config_get('csv', 'pdw_prefix')
+
+        if not os.path.isdir(self.watch_dir):
+            self.ds.logger.error('Directory does not exist: %s' %self.watch_dir)
+            return
+
+        if not os.path.isdir(self.backup_dir):
+            self.ds.logger.error('Directory does not exist: %s' %self.backup_dir)
+            return
+
+
+        self.last_run = self.ds.get_state(self.state_dir)
+        self.time_format = "%Y-%m-%dT%H:%M:%S"
+        self.limit = 100
+        current_time = datetime.utcnow()
+        self.current_run = current_time.strftime(self.time_format)
+
+        if self.last_run == None:
+            last_run = current_time - timedelta(hours = 8)
+            self.last_run = last_run.strftime(self.time_format)
+
+        if not self.checkDirectory():
+            self.ds.logger.error('Failed in data run')
+        return
+
+        module_list = ['architect', 'dna', 'secure-now']
+        work_list = []
+        if self.restart_module == None:
+            work_list = module_list
+        else:
+            if self.restart_module not in module_list:
+                self.ds.log('ERROR', 'Invalid module specified: ' + self.restart_module)
+                return None
+            found = False
+            for item in module_list:
+                if not found and self.restart_module != item:
+                    continue
+                else:
+                    found = True
+                work_list.append(item) 
+                
+        self.ds.log('INFO', 'Handling modules: ' + str(work_list))
+
+        for event_type in work_list:
+            if not self.get_csvData(event_type=event_type):
+                self.ds.log('Error', "Something went wrong for event type " + event_type + ".  Check logs above")
+            #else:
+                #for log in event_logs:
+                    #self.ds.writeJSONEvent(log, JSON_field_mappings = self.JSON_field_mappings)
+
+        self.ds.set_state(self.state_dir, self.current_run)
+        self.ds.log('INFO', "Done Sending Notifications")
+
+
+    def run(self):
+        try:
+            pid_file = self.ds.config_get('csv', 'pid_file')
+            fp = open(pid_file, 'w')
+            try:
+                fcntl.lockf(fp, fcntl.LOCK_EX | fcntl.LOCK_NB)
+            except IOError:
+                self.ds.log('ERROR', "An instance of csv is already running")
+                # another instance is running
+                sys.exit(0)
+            self.csv_main()
+        except Exception as e:
+            traceback.print_exc()
+            self.ds.logger.error("Exception {0}".format(str(e)))
+            return
+    
+    def usage(self):
+        print
+        print(os.path.basename(__file__))
+        print
+        print('  No Options: Run a normal cycle')
+        print
+        print('  -t    Testing mode.  Do all the work but do not send events to GRID via ')
+        print('        syslog Local7.  Instead write the events to file \'output.TIMESTAMP\'')
+        print('        in the current directory')
+        print
+        print('  -l    Log to stdout instead of syslog Local6')
+        print
+        print('  -c <counter>  Starting point for restarting run')
+        print
+        print('  -m <module>  Moudle to restart at after errors')
+        print('               NOTE: Modules run in order of:')
+        print('                    architect, dna, secure-now')
+        print('               script will continue from specified starting point')
+        print
+    
+    def __init__(self, argv):
+
+        self.testing = False
+        self.send_syslog = True
+        self.ds = None
+        self.restart_counter = None
+        self.restart_module = None
+    
+        try:
+            opts, args = getopt.getopt(argv,"htnlc:m:",["datedir="])
+        except getopt.GetoptError:
+            self.usage()
+
+            sys.exit(2)
+        for opt, arg in opts:
+            if opt == '-h':
+                self.usage()
+                sys.exit()
+            elif opt in ("-t"):
+                self.testing = True
+            elif opt in ("-l"):
+                self.send_syslog = False
+            elif opt in ("-c"):
+                self.restart_counter = arg
+            elif opt in ("-m"):
+                self.restart_module = arg
+        if (self.restart_counter == None and self.restart_module != None) or \
+            (self.restart_counter != None and self.restart_module == None):
+            print()
+            print('-c and -m need to be used together')
+            print()
+            self.usage()
+            sys.exit(2)
+        try:
+            self.ds = DefenseStorm('csvEvents', testing=self.testing, send_syslog = self.send_syslog)
+        except Exception as e:
+            traceback.print_exc()
+            try:
+                self.ds.log('ERROR', 'ERROR: ' + str(e))
+            except:
+                pass
+
+
+if __name__ == "__main__":
+    i = integration(sys.argv[1:]) 
+    i.run()
